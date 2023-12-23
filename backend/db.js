1c34a0d747c5319aa535753b8e15f177c30eee47
Utkarsh <utkarshshrivastava.13@gmail.com>
2023-12-23 12:33:56 +0530

    Commit_03[created db.js,connected to mongodb, nodemon added]

---
+++ backend/db.js
@@ -0,0 +1,18 @@
+// db.js
+const mongoose = require("mongoose");
+const mongoURI = "mongodb://localhost:27017"; // Replace 'your-database-name' with your actual database name
+
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("Connected to MongoDB successfully");
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+  }
+};
+
+module.exports = connectToMongo;
