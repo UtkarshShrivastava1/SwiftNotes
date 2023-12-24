2e9e271d3781d376a0bc9078f1e9f46caccedac3
Utkarsh <utkarshshrivastava.13@gmail.com>
2023-12-24 21:30:52 +0530

    Commit_07[bcryptjs  and jwt added]

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,11 @@
 const User = require("../models/User");
 const router = require("express").Router();
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 router.post(
-  "/",
+  "/createuser",
   [
     // Validate name
     body("name").trim().isLength({ min: 1 }).withMessage("Name is required"),
@@ -20,8 +22,6 @@ router.post(
   ],
   async (req, res) => {
     try {
-      console.log(req.body);
-
       // Check for validation errors
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
@@ -34,11 +34,24 @@ router.post(
         return res.status(400).json({ error: "Email already exists" });
       }
 
-      // Use User.create for a more concise code
-      const newUser = await User.create(req.body);
+      // Hash the password before saving it
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-      // Send the entire user object in the response
-      res.json(newUser.toObject());
+      // Create a new user with the hashed password
+      const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+      });
+
+      // Create a JWT token
+      const token = jwt.sign({ userId: newUser._id }, "your-secret-key", {
+        expiresIn: "1h",
+      });
+
+      // Send only the token in the response
+      res.json({ token });
     } catch (error) {
       console.error("Error saving user:", error);
       res.status(500).json({ error: "Internal Server Error" });
